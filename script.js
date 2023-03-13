9e60aabb6cd605f54adb5320db82868e92eb5356
maykhai <127709805+maykhai@users.noreply.github.com>
2023-03-13 11:19:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//while loop
+
+var a = 0;
+while(a<60){
+	a=a+2;
+	document.writeln(a);
+}
+
+//do while loop
+
+var b=0;
+do{
+	b=b+3;
+	document.writeln(b);
+}
+while(b<60)
+
+
+//loop
+var w=0;
+for(w=5; w<51; w=w+5){
+	document.writeln(w);
+}
